db73a617c4b327fd3edab303e34dc4af390ebd8b
DandiKurnia <dandikurnia608@gmail.com>
2022-09-08 15:39:47 +0700

    dashboard

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Stasiun;
+use App\Models\Customer;
+use App\Models\Ticket;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $stasiun = Stasiun::count();
+        $customer = Customer::count();
+        $ticket = Ticket::count();
+        return view('dashboard', compact('stasiun', 'customer', 'ticket'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Stasiun  $stasiun
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Stasiun $stasiun)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Stasiun  $stasiun
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Stasiun $stasiun)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Stasiun  $stasiun
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Stasiun $stasiun)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Stasiun  $stasiun
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Stasiun $stasiun)
+    {
+        //
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,57 @@
 @extends('layouts.partials.main')
 
 @section('container')
-    <p>ini dashboard</p>
+<div class="row">
+    <div class="col-xl-3 col-lg-6">
+        <div class="card card-stats mb-4 mb-xl-0">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col">
+                        <h5 class="card-title text-uppercase text-muted mb-2">Stasiun</h5>
+                        <span class="h2 font-weight-bold">{{ $stasiun }}</span>
+                    </div>
+                    <div class="col-auto">
+                        <div class="icon">
+                            <i class="menu-icon tf-icons bx bx-train"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-lg-6">
+        <div class="card card-stats mb-4 mb-xl-0">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col">
+                        <h5 class="card-title text-uppercase text-muted mb-2">customer</h5>
+                        <span class="h2 font-weight-bold">{{ $customer }}</span>
+                    </div>
+                    <div class="col-auto">
+                        <div class="icon">
+                            <i class='bx bx-user'></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-lg-6">
+        <div class="card card-stats mb-4 mb-xl-0">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col">
+                        <h5 class="card-title text-uppercase text-muted mb-2">Ticket</h5>
+                        <span class="h2 font-weight-bold">{{ $ticket }}</span>
+                    </div>
+                    <div class="col-auto">
+                        <div class="icon">
+                            <i class='bx bx-credit-card-front'></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
